af68cce1265ad6a1781d80d5eb0e97741366258b
yuseonjun <seonjun0906@gmail.com>
2023-09-05 21:23:45 +0900

    feat: 스크린을 담당하는 Enum 생성

---
+++ src/main/java/subway/enums/Screen.java
@@ -0,0 +1,34 @@
+package subway.enums;
+
+import java.util.List;
+
+public enum Screen {
+
+    MAIN("B", "메인 화면", List.of("1. 역 관리", "2. 노선 관리", "3. 구간 관리", "4. 지하철 노선도 출력", "Q. 종료")),
+    LINE("1", "역 관리", List.of("1. 역 등록", "2. 역 삭제", "3. 역 조회", "B. 돌아가기")),
+    STATION("2", "노선 관리", List.of("1. 노선 등록", "2. 노선 삭제", "3. 노선 조회", "B. 돌아가기")),
+    PATH("3", "구간 관리", List.of("1. 구간 등록", "2. 구간 삭제", "B. 돌아가기")),
+    SUBWAY_MAP("4", "지하철 노선도 출력", List.of());
+
+    private final String index;
+    private final String title;
+    private final List<String> functions;
+
+    Screen(String index, String title, List<String> functions) {
+        this.index = index;
+        this.title = title;
+        this.functions = functions;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<String> getFunctions() {
+        return functions;
+    }
+}
---
+++ src/main/java/subway/enums/SubwayMap.java
@@ -0,0 +1,5 @@
+package subway.enums;
+
+public class SubwayMap {
+
+}
